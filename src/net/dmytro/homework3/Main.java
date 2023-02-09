2d7c515c08553161b60987e64138f28a9e94a3e6
Your Name <you@example.com>
2023-02-09 19:53:55 +0200

    added to gitignore out, deleted object from Car

---
+++ src/net/dmytro/homework3/Main.java
@@ -0,0 +1,8 @@
+package net.dmytro.homework3;
+
+public class Main {
+    public static void main(String[] args) {
+        Employee employee = new Employee("Jack","Capitan", "driver", "test@gmail.com","2312312312312", 23);
+        System.out.println(employee.personIfo());
+    }
+}
